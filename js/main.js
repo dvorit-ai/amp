2b13eed82d1e24268b72c3bf7527ec4e38775436
dvorit-ai <dkhodanovich@gmail.com>
2020-03-11 19:11:02 -0400

    js update

--- js/main.js
+++ js/main.js
@@ -8,4 +8,15 @@
 				})
 		})
 
+$(".close").click(function(){
+	$(this)
+		.parent(".hidden")
+		.attr("style","")
+		.css({
+		"display":"none"
+		})
 
+	// setTimeout(function(){
+	// 	$(".hidden").attr("style","")
+	// },500)
+	})
