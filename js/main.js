15c46439c2b4ef90f68b9ffb70f45bb960eac8d6
dvorit-ai <dkhodanovich@gmail.com>
2020-03-24 17:00:57 -0400

    corona failure

--- js/main.js
+++ js/main.js
@@ -20,3 +20,65 @@ $(".close").click(function(){
 	// 	$(".hidden").attr("style","")
 	// },500)
 	})
+
+	
+//start of virus 
+
+$(".virus").each(function(){
+	randomTop = Math.floor(Math.random()*$(window).height())
+	randomLeft = Math.floor(Math.random()*$(window).width())
+	randomSpeed = Math.floor(Math.random()*400)+300
+
+	$(this)
+		.css({
+			"top":randomTop,
+			"left":randomLeft
+		})
+		.marqueeify({
+			"speed":randomSpeed
+		});
+
+})
+
+//end of virus 
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   animateDiv();
+
+// });
+
+// function makeNewPosition() {
+
+//   // Get viewport dimensions (remove the dimension of the div)
+//   var h = $(window).height() - 50;
+//   var w = $(window).width() - 50;
+
+//   var nh = Math.floor(Math.random() * h);
+//   var nw = Math.floor(Math.random() * w);
+
+//   return [nh, nw];
+
+// }
+
+// function animateDiv() {
+//   var newq = makeNewPosition();
+//   $('.a').animate({
+//     top: newq[0],
+//     left: newq[1]
+//   },1300, function() {
+//     animateDiv();
+//   });
+
+// };
+
+
+
+
+
